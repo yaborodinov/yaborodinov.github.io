9b396896ea426ce023da00fab4d5a25176b61163
Alexandr <yaborodinov@gmail.com>
2020-06-24 16:34:49 +0300

    add class /wish-active/ with js

--- shop project/app.js
+++ shop project/app.js
@@ -8,3 +8,32 @@ for (let i = 0; i < addToCartButtons.length;i++){
         productsCountEl.textContent = prevProductCounts + 1;
     })
 }
+
+
+
+
+let wish = document.querySelector("#device-1");
+function changeImg() {
+    let imgBgd = imgEl.getAttribute("src");
+    if (imgSrc === "img/sdfsdfsdf.png") {
+        imgEl.setAttribute("src", "img/hg.png")
+    } else if (imgSrc === "img/sdfsdfsdf.png" || imgSrc === "img/1.jpg") {
+        imgEl.setAttribute("src", "img/sdfsdfsdf.png")
+    } else {
+        imgEl.setAttribute("src", "img/1.jpg")
+    }
+    imgEl.addEventListener("click", changeImg);
+}
+
+
+// wish=======
+let wishElement = document.querySelectorAll(".wish");
+for (let i = 0; i < wishElement.length; i++){
+    wishElement[i].addEventListener("click", function () {
+        if (wishElement[i].className == "wish wish-active") {
+            wishElement[i].classList.remove("wish-active")
+        } else {
+            wishElement[i].classList.add("wish-active")
+        }
+    })
+}
