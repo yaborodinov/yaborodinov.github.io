a132e1029bdc92e8dbab2e642b0e3f2168888a40
Alexandr <yaborodinov@gmail.com>
2020-07-08 14:14:39 +0300

    made second page/ start doing first section in it

---
+++ exam/js/secondPage.js
@@ -0,0 +1,9 @@
+"use strict"
+$(document).ready(function () {
+    $('.quote-slider').slick({
+        mobileFirst: true,
+        arrows: false,
+        // autoplay: true,
+        // autoplaySpeed: 1000
+    });
+    });
